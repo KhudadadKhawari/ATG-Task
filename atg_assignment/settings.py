b901e52834fdcc627461c7faf2a12ae9ad42a41d
Khudadad Khawari <Rayan.Khawari.01@gmail.com>
2022-10-24 22:01:46 +0530

    login required

--- atg_assignment/settings.py
+++ atg_assignment/settings.py
@@ -147,3 +147,7 @@ AUTH_USER_MODEL = "account.User"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = 'user_login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
